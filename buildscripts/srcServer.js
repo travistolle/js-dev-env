96789848d1294a3ce351366b5f5c17b9f5bbe2f8
Lake Travis <danielbenner@gmail.com>
2018-12-15 22:26:01 -0600

    turned on watch for eslint, added lint:watch to start script

--- buildscripts/srcServer.js
+++ buildscripts/srcServer.js
@@ -2,8 +2,8 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
-import config from '../webpack.config.dev';
-
+import config from '../webpack.config.dev.js';
+/* eslint-disable no-console */
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './index.css';
 import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log(`Daniel won the lottery for ${courseValue} this month!`);
+// debugger;
+console.log(`Daniel won the lottery for ${courseValue} this month!`);  // eslint-disable-line no-console
+

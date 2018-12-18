cde0424f19737aaab025c2dae4629fbcc45773a4
Lake Travis <danielbenner@gmail.com>
2018-12-17 18:57:32 -0600

    final updates to the js-dev-env, deploy pipeline with Heroku and Surge

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://sheltered-chamber-37453.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
